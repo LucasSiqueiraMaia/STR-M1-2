222d9a266a4a474cc0e08d80334b4628098dea9d
LucasSiqueiraMaia <lucas.siqmaia@gmail.com>
2026-09-23 07:46:02 -0300

    Escopo do projeto com Espressif

---
+++ EsteiraIndustrial/build/bootloader/ld/bootloader.memory.ld.d
@@ -0,0 +1,3 @@
+/home/lucas/Projects/C/STR-M1-2/EsteiraIndustrial/build/bootloader/ld/bootloader.memory.ld: \
+ /home/lucas/esp/esp-idf/components/bootloader/subproject/main/ld/esp32/bootloader.memory.ld.in \
+ /home/lucas/Projects/C/STR-M1-2/EsteiraIndustrial/build/bootloader/config/sdkconfig.h
